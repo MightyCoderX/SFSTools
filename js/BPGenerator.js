61f566a30daf6ad7d1901d85737315e0a642f233
MightyCoderX <mattiaca003@gmail.com>
2020-08-08 01:56:46 +0200

    Finished Generating Html Templates

--- js/BPGenerator.js
+++ js/BPGenerator.js
@@ -2,6 +2,6 @@ const parts = {
     engine: ["Engine Hawk", "Engine Valiant", "Engine Kolibri", "Engine Titan", "Engine Frontier", "Ion Engine"],
     solar_array: [2, 3],
     aerodynamics: ["Side Fuselage", "Cone", "Cone Side"],
-    wheel: ["Wheel Medium", "Wheel Large"]
+    wheel: ["Wheel Medium", "Wheel Big"],
     parachute: ["Parachute", "Parachute Side"]
 }
