2237a7c0a0f9417381908b8cc7edfa7f84390bbb
MightyCoderX <mattiaca003@gmail.com>
2020-08-08 01:31:43 +0200

    Bunch O' Things

--- js/BPGenerator.js
+++ js/BPGenerator.js
@@ -1,8 +1,7 @@
 const parts = {
     engine: ["Engine Hawk", "Engine Valiant", "Engine Kolibri", "Engine Titan", "Engine Frontier", "Ion Engine"],
     solar_array: [2, 3],
-    aerodynamics: [""],
-    wheel: [],
-    fairing: [],
-    docking_port: []
+    aerodynamics: ["Side Fuselage", "Cone", "Cone Side"],
+    wheel: ["Wheel Medium", "Wheel Large"]
+    parachute: ["Parachute", "Parachute Side"]
 }
