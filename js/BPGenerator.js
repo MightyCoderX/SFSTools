d941493aff61556dbe1f40deea65bb01f7a32a5a
MightyCoderX <mattiaca003@gmail.com>
2020-08-08 00:19:58 +0200

    Updated

--- js/BPGenerator.js
+++ js/BPGenerator.js
@@ -1,4 +1,8 @@
 const parts = {
     engine: ["Engine Hawk", "Engine Valiant", "Engine Kolibri", "Engine Titan", "Engine Frontier", "Ion Engine"],
-    solar_array: [2, 3, ]
+    solar_array: [2, 3],
+    aerodynamics: [""],
+    wheel: [],
+    fairing: [],
+    docking_port: []
 }
