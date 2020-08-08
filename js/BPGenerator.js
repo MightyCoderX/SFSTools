96ef9b55588df7dadea5810e72cb0bc48b63980a
MightyCoderX <mattiaca003@gmail.com>
2020-08-08 02:36:14 +0200

    Adding feature to add parts

--- js/BPGenerator.js
+++ js/BPGenerator.js
@@ -1,3 +1,6 @@
+const partType = document.getElementById('partType');
+const btnAddPart = document.getElementById('btnAddPart');
+
 const parts = {
     engine: ["Engine Hawk", "Engine Valiant", "Engine Kolibri", "Engine Titan", "Engine Frontier", "Ion Engine"],
     solar_array: [2, 3],
@@ -5,3 +8,9 @@ const parts = {
     wheel: ["Wheel Medium", "Wheel Big"],
     parachute: ["Parachute", "Parachute Side"]
 }
+
+btnAddPart.addEventListener('click', async () =>
+{
+    const file = partType;
+    console.log(file);
+});
