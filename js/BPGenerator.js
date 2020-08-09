6370be5b9bb4ffd421aa1756f8ad38019384781e
MightyCoderX <mattiaca003@gmail.com>
2020-08-09 02:16:53 +0200

    Implementing main functionality

--- js/BPGenerator.js
+++ js/BPGenerator.js
@@ -23,10 +23,17 @@ btnAddPart.addEventListener('click', () =>
     .then(res  => res.text())
     .then(data =>
     {
-       
-
+        if(parts[value])
+        {
+            console.log(insertAtIndex(data, data.indexOf("\n"), "Hello"));
+        }
 
         partsElem.insertAdjacentHTML('beforeend', data);
         console.log(data);
     });
 });
+
+function insertAtIndex(str, index, text)
+{
+    return str.substring(0, index) + text + str.substring(index+text.length-1);
+}
